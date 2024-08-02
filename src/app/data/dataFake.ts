b8cc2747f5fda438339341003428d49f1f124dde
ViniciusBaroni22 <baronivinicius_2201@hotmail.com>
2024-08-02 19:22:01 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"ILHABELA",
+    "description":"Ilhabela um paraíso Brasileiro",
+     "photoCover":"https://i.pinimg.com/564x/fe/73/4c/fe734c542cd08f28d31121712e591658.jpg"
+  },
+  {
+
+    "id":"2",
+    "title":"PRAIA DO BONETE",
+    "description":"BLZBLZBZLZBZLZZ",
+    "photoCover":"https://i.pinimg.com/564x/2d/cb/f0/2dcbf0f114e6ec7bb2231319194ca472.jpg"
+
+  }
+]
